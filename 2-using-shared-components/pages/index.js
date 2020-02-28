9d169510f72e7d4217a9222ff086494c79f19442
windsdeath <windsdeath@gmail.com>
2020-02-28 09:33:36 +0900

    next pretice

--- 2-using-shared-components/pages/index.js
+++ 2-using-shared-components/pages/index.js
@@ -1,12 +1,9 @@
-import Link from 'next/link'
+import Layout from '../coms/MyLayout';
 
 export default function Index() {
   return (
-    <div>
-      <Link href="/about">
-        <a>About Page</a>
-      </Link>
-      <p>Hello Next.js</p>
-    </div>
-  )
+    <Layout>
+    <p>Hello Next.js</p>
+  </Layout>
+  );
 }
---
+++ 3-create-dynamic-pages/pages/post.js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import Layout from '../components/MyLayout';
+
+const Page = () => {
+  const router = useRouter();
+  return (
+    <Layout>
+      <h1>{router.query.titlee}</h1>
+      <p>This is the blog post content.</p>
+    </Layout>
+  );
+};
+
+export default Page;
